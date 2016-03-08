5c397d9e376fa05addef5c030081bfcfbae3bf8e
Jessica Murphy <jessicamurphy.ma@gmail.com>
2016-03-07 18:29:37 -0700

    Class Notes Monday 307

---
+++ class7_practice302.rb
@@ -0,0 +1,16 @@
+# Define a method with one argument - number
+# Check if that number is even or not
+# Display the result
+
+def number(x)
+x % 2 == 0 ? puts("you are even") : puts ("you are odd")
+end
+#   if x % 2 ==0
+#     puts "You are even"
+#   else
+#     puts "You are odd"
+#   end
+# end
+
+# number(2)
+# number(3)
